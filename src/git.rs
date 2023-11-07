6a13e280c72ab7f50a6836f051aea19c21039397
thevickypedia <vignesh.sivanandarao@gmail.com>
2023-11-06 19:12:38 -0600

    Enable logging and run startup git commands

---
+++ src/git.rs
@@ -0,0 +1,18 @@
+use std::process::{Command, Stdio};
+use log::debug;
+
+pub fn run(command: &str) -> bool {
+    let output = Command::new("sh")  // invoke a shell
+        .arg("-c")  // execute command as interpreted by program
+        .arg(command)  // run the command
+        .stdout(Stdio::null())  // Redirect stdout to /dev/null
+        .stderr(Stdio::null())  // Redirect stderr to /dev/null
+        .status()  // check for status
+        .expect("Failed to execute command");
+    if output.success() {
+        true
+    } else {
+        debug!("Command failed with an error code: {:?}", output.code());
+        false
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
+extern crate log;
+
+use std::env;
+
+use log::{info, warn};
+
 mod parse;
+mod git;
 
 fn main() {
     let config = parse::arguments();
+    // todo: remove print statements
     println!("Reverse: {}", config.reverse);
     println!("Debug: {}", config.debug);
     println!("Filename: {}", config.filename);
     println!("Title: {}", config.title);
+    // logger will be enabled only or debug mode
+    if config.debug {
+        env::set_var("RUST_LOG", "debug");
+        env_logger::init();
+    }
+    if git::run("git fetch origin refs/tags/*:refs/tags/* --prune") &&
+        git::run("git pull") {
+        info!("Tags updated")
+    } else {
+        warn!("Failed to refresh tags");
+    }
 }

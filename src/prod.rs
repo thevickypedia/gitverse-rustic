950fbc2cf42884d0faa38429d681f7a2c70f040f
thevickypedia <vignesh.sivanandarao@gmail.com>
2023-11-08 13:02:58 -0600

    Remove redundancy in temp fix

--- src/prod.rs
+++ src/prod.rs
@@ -1,7 +1,6 @@
 use draft;
 
 pub fn generate_snippets(reverse: bool) -> Vec<String> {
-    // todo: find a better solution than ``.replace('"', "")``
     let mut snippets = vec![];
     for each_tag in draft::generator(reverse).unwrap() {
         let line1 = format!("{} ({})",
@@ -9,16 +8,16 @@ pub fn generate_snippets(reverse: bool) -> Vec<String> {
                             each_tag.get("date").unwrap());
         let mut description = vec![];
         for desc in each_tag.get("description").unwrap().as_array().unwrap() {
-            let desc_str = desc.to_string().replace('"', "");
+            let desc_str = desc.to_string().replace('"', "");  // todo: temp fix
             if desc_str.starts_with("-") {
                 description.push(desc_str)
             } else {
                 description.push(format!("- {}", desc_str))
             }
         }
-        let line1 = line1.replace('"', "");
+        let line1 = line1.replace('"', "");  // todo: temp fix
         let line2 = "-".repeat(line1.len());
-        let line3 = description.join("\n").replace('"', "");
+        let line3 = description.join("\n");
         let line = format!("{}\n{}\n{}\n", line1, line2, line3);
         snippets.push(line);
     }

80a4d183f98c4b23e79edbfa45de7df86de49f13
thevickypedia <vignesh.sivanandarao@gmail.com>
2023-11-08 13:00:05 -0600

    Add a temporary fix for doubly quoted strings from `Value`

--- src/prod.rs
+++ src/prod.rs
@@ -1,23 +1,24 @@
 use draft;
 
 pub fn generate_snippets(reverse: bool) -> Vec<String> {
-    // todo: figure out why function returns doubly quoted strings (makes file look ugly)
+    // todo: find a better solution than ``.replace('"', "")``
     let mut snippets = vec![];
     for each_tag in draft::generator(reverse).unwrap() {
         let line1 = format!("{} ({})",
                             each_tag.get("version").unwrap(),
                             each_tag.get("date").unwrap());
-        let line2 = "-".repeat(line1.len());
         let mut description = vec![];
         for desc in each_tag.get("description").unwrap().as_array().unwrap() {
-            let desc_str = desc.to_string();
+            let desc_str = desc.to_string().replace('"', "");
             if desc_str.starts_with("-") {
                 description.push(desc_str)
             } else {
                 description.push(format!("- {}", desc_str))
             }
         }
-        let line3 = description.join("\n");
+        let line1 = line1.replace('"', "");
+        let line2 = "-".repeat(line1.len());
+        let line3 = description.join("\n").replace('"', "");
         let line = format!("{}\n{}\n{}\n", line1, line2, line3);
         snippets.push(line);
     }

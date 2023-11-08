d1acf143ea7bdde57e62d06bf85c708eb45d8088
thevickypedia <vignesh.sivanandarao@gmail.com>
2023-11-08 12:39:51 -0600

    Remove unnecessary conditions on draft notes generation

--- src/draft.rs
+++ src/draft.rs
@@ -22,30 +22,27 @@ pub fn generator(reverse: bool) -> Option<Vec<Map<String, Value>>> {
     }
     info!("Git tags gathered: {}", tag_notes.len());
     let release_notes = releases::get();
+    if !release_notes.is_some() { return Some(tag_notes); }
     let mut updated_tags = Vec::new();
-    if !release_notes.is_none() {
-        let bind_release_api = release_notes.unwrap();
-        if !bind_release_api.is_empty() {
-            info!("Release notes gathered: {}", bind_release_api.len());
-            for mut tag in tag_notes.clone() {
-                let tag_version = tag.get("version").unwrap().as_str().unwrap();
-                let api_desc = bind_release_api.get(tag_version);
-                // Check if the release version and tag name are the same
-                if api_desc.is_some() {
-                    let bind_api_desc = api_desc.unwrap();
-                    debug!("'{}' -> '{:?}'", tag.get("description").unwrap(), bind_api_desc);
-                    let mut description = vec![];
-                    for desc in bind_api_desc {
-                        description.push(Value::String(desc.to_string()))
-                    }
-                    // Overwrite the value of "description" key for all the tags
-                    tag.insert("description".to_string(), Value::Array(description));
-                } else {
-                    warn!("Tag name: '{}' could not be found in releases", tag_version)
-                }
-                updated_tags.push(tag)
+    let bind_release_api = release_notes.unwrap();
+    info!("Release notes gathered: {}", bind_release_api.len());
+    for mut tag in tag_notes.clone() {
+        let tag_version = tag.get("version").unwrap().as_str().unwrap();
+        let api_desc = bind_release_api.get(tag_version);
+        // Check if the release version and tag name are the same
+        if api_desc.is_some() {
+            let bind_api_desc = api_desc.unwrap();
+            debug!("'{}' -> '{:?}'", tag.get("description").unwrap(), bind_api_desc);
+            let mut description = vec![];
+            for desc in bind_api_desc {
+                description.push(Value::String(desc.to_string()))
             }
+            // Overwrite the value of "description" key for all the tags
+            tag.insert("description".to_string(), Value::Array(description));
+        } else {
+            warn!("Tag name: '{}' could not be found in releases", tag_version)
         }
+        updated_tags.push(tag)
     }
     return if updated_tags.is_empty() { Some(tag_notes) } else { Some(updated_tags) };
 }
